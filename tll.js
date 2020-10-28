6a9f00d7cce91dbe7e959fd800e6902db3c80823
hlarsonAE <hlarson@autoeurope.com>
2020-10-27 21:28:23 -0400

    Fixed a typo in the assert. Added explanatory comments.

--- tll.js
+++ tll.js
@@ -6,10 +6,11 @@ let exerciseUrl = "https://translifeline.org/hiring_stats_sample";
 async function main() {
 	let data = await getHiringData(exerciseUrl);
 	displayHiringData(data);
+	return data;
 }
 
 function displayHiringData(data) {
-	assert('answered' in data, "The data does not have a field called 'answer'.")
+	assert('answered' in data, "The data does not have a field called 'answered'.")
 	console.log(formatNumber(data.answered));
 }
 
@@ -22,10 +23,11 @@ async function getHiringData(url) {
 function formatNumber(number, segmentLength=3) {
 	let numberString = number.toString();
 	let out = "";
+
 	while(numberString.length > 0) {
 		if(numberString.length > segmentLength) {
-			out = `,${numberString.slice(-segmentLength)}${out}`;
-			numberString = numberString.slice(0, numberString.length - segmentLength);
+			out = `,${numberString.slice(-segmentLength)}${out}`; //add the last {segmentLength} characters to the front of the output string
+			numberString = numberString.slice(0, numberString.length - segmentLength); //chop the last three characters off of the numberString
 		} else {
 			out = `${numberString}${out}`;
 			numberString = "";
@@ -34,7 +36,10 @@ function formatNumber(number, segmentLength=3) {
 	return out;
 }
 
-main().then(null,(error) => {
-	console.log("Error Occurred", error);
-});
+main().then(
+	null,// if you need to do something AFTER main finishes, put that callback function here instead of null. the retrieved data will be passed as the parameter
+	(error) => { 
+		console.log("Error Occurred", error);
+	}
+);
 
